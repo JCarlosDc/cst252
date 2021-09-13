fa841324feb6e1ebeafc96882ddba3f9b914d14d
JCarlosDc <jdelcarmen@csumb.edu>
2021-09-13 15:10:02 -0700

    Lab 5 Update

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+/**
+ * Author: Carlos Salazar <carsalazar@csumb.edu>
+ * Created: September 13, 2021
+ * License: Public Domain
+ */
+ //Define variables
+var make = "Honda";
+var model = "civic";
+var color = "grey";
+var year = 2008;
+
+// calculate
+var age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " year<br>");
